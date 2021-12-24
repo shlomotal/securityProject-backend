400c8356ae832777c232326a69b17d081da8b847
saar salhov <saarsalhov@gmail.com>
2021-12-24 12:06:55 +0200

    Add complex password

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require("express"),
   router = express.Router();
 const bcrypt = require("bcryptjs");
 const config = require("config");
+const match = require("nodemon/lib/monitor/match");
 const general = require("../modules/general");
 
 // get user lists
@@ -17,6 +18,8 @@ router.get("/getAll", async function (req, res) {
 
 // create new user
 router.post("/signup", async function (req, res) {
+  var complexPassword = config.get("complexPassword");
+  var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{" + config.get("passwordLength") + ",})");
   var errors = [];
   var con = general.getConn();
   console.log(
@@ -24,10 +27,17 @@ router.post("/signup", async function (req, res) {
     req.body.password,
     req.body.confirmPassword
   );
-  if (req.body.password < config.get("passwordLength")) {
-    console.log("password:", req.body.password);
-    errors.push("Password must be at least 10 characters");
+  if (complexPassword){
+    if(!strongRegex.test(req.body.password)){
+      res.status(400).send(JSON.stringify({ error: "You chose complex password and its to weak" }));
+      return false;
+    }
+    console.log("Great! its complex password")
   }
+ // if (req.body.password < config.get("passwordLength")) {
+ //   console.log("password:", req.body.password);
+  //  errors.push("Password must be at least 10 characters");
+  //}
   if (req.body.confirmPassword !== req.body.password) {
     console.log("Passwords do not match");
     errors.push("Passwords do not match");
@@ -145,14 +155,15 @@ router.post("/login", async (req, res) => {
 });
 
 //Change password
-router.post("/changePass", async function (req, res) {
+router.post("/changePass", async function (req, res)
+{
+  var complexPassword = config.get("complexPassword");
+  var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{" + config.get("passwordLength") + ",})");
   var con = general.getConn();
   var errors = [];
-  responseUsername = await con
-    .promise()
-    .query("select count(*) as cnt from users where username=?", [
-      req.body.username,
-    ]);
+  responseUsername = await con.promise().query("select count(*) as cnt from users where username=?", [req.body.username]);
+  responsePassword = await con.promise().query("select password from users where username=?", [req.body.username]);
+  console.log("req:" + req.body.username, req.body.oldPassword, req.body.newPassword ,req.body.confirmNewPassword);
   console.log("exist: ", responseUsername[0][0].cnt);
   if (responseUsername[0][0].cnt === 0) {
     res.status(400);
@@ -160,34 +171,30 @@ router.post("/changePass", async function (req, res) {
     con.end();
     return false;
   }
-  responsePassword = await con
-    .promise()
-    .query("select password from users where username=?", [req.body.username]);
-  console.log(
-    "req:" + req.body.username,
-    req.body.oldPassword,
-    req.body.newPassword,
-    req.body.confirmNewPassword
-  );
   console.log("responsePassword: ", responsePassword[0][0].password);
   console.log("req.body.oldPassword: ", req.body.oldPassword);
   const validPass = await bcrypt.compare(
     req.body.oldPassword,
     responsePassword[0][0].password
   );
-  console.log("valid password: " + validPass);
-  if (!validPass) {
+  console.log('valid password: ' + validPass);
+  if (!validPass)
+  {
     res.status(400).send(JSON.stringify({ error: "Incorrect Password" }));
     con.end();
     return false;
   }
-  if (req.body.newPassword < config.get("passwordLength")) {
-    console.log("password:", req.body.password);
-    errors.push("Password must be at least 10 characters");
-  }
   if (req.body.newPassword !== req.body.confirmNewPassword) {
     console.log("Passwords do not match");
     errors.push("Passwords do not match");
+  }
+  if (complexPassword){
+    if(!strongRegex.test(req.body.newPassword)){
+      res.status(400).send(JSON.stringify({ error: "You chose complex password and its to weak" }));
+      con.end();
+      return false;
+    }
+    console.log("Great! its complex password")
   }
   console.log(config.get("passwordDictonary"));
   if (config.get("passwordDictonary")) {
@@ -206,6 +213,7 @@ router.post("/changePass", async function (req, res) {
       errors.push("Password in dictonary_passwords");
     }
   }
+
   if (errors.length !== 0) {
     res.status(400).send(JSON.stringify({ error: "Incorrect Password" }));
     con.end();
@@ -216,20 +224,44 @@ router.post("/changePass", async function (req, res) {
   console.log("salt: " + salt);
   const hashedPassword = await bcrypt.hash(req.body.newPassword, salt);
   console.log("hashedPassword: " + hashedPassword);
-  var createQuery = await con
-    .promise()
-    .query("UPDATE users SET password=? WHERE username=?", [
-      hashedPassword,
-      req.body.username,
-    ]);
+  var createQuery = await con.promise().query("UPDATE users SET password=? WHERE username=?", [hashedPassword, req.body.username]);
   console.log("createdQuery: " + createQuery[0].insertId);
 
   userId = createQuery[0].insertId;
-  res.status(200).send(
-    JSON.stringify({
-      status: "password changed successfully. userId:" + userId,
-    })
-  );
+  res.status(200).send(JSON.stringify({ status: "password changed successfully. userId:" + userId }));
+
 });
+
+	// JavaScript program to check if the string
+	// contains uppercase, lowercase
+	// special character & numeric value
+
+	function isAllPresent(i_password) {
+		// Regex to check if a string
+		// contains uppercase, lowercase
+		// special character & numeric value
+		var pattern = new RegExp(
+		"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[-+_!@#$%^&*.,?]).+$"
+		);
+
+		// If the string is empty
+		// then print No
+		if (!i_password || i_password.length === 0) {
+		document.write("No" + "<br>");
+		return;
+		}
+
+		// Print Yes If the string matches
+		// with the Regex
+		if (pattern.test(i_password)) {
+		document.write("Yes" + "<br>");
+		} else {
+		document.write("No" + "<br>");
+		}
+		return;
+	}
+
+
+
 
 module.exports = router;
