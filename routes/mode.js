ccaf4aa89def45664e12bf4f5b0d586e31e1d1e1
Tal Shlomo <shlomotal01@gmail.com>
2022-01-04 17:43:45 +0200

    Add isSecure variable to modes API

--- routes/mode.js
+++ routes/mode.js
@@ -5,7 +5,7 @@ const express = require("express"),
 router.post("/notSecured", async function (req, res) {
   var fs = require("fs");
   var updatedJson =
-    '{\n"passwordLength": 0,\n"complexPassword": false,\n"passwordHistoryLength": 0,\n"passwordDictonary": false,\n"loginRetries": 0,\n"lockTimeInMinutes": 0\n}';
+    '{\n"isSecured": false,\n"passwordLength": 0,\n"complexPassword": false,\n"passwordHistoryLength": 0,\n"passwordDictonary": false,\n"loginRetries": 0,\n"lockTimeInMinutes": 0\n}';
 
   fs.writeFile("config/default.json", updatedJson, function (err) {
     if (err) throw err;
@@ -20,7 +20,7 @@ router.post("/notSecured", async function (req, res) {
 router.post("/secured", async function (req, res) {
   var fs = require("fs");
   var updatedJson =
-    '{\n"passwordLength": 10,\n"complexPassword": true,\n"passwordHistoryLength": 3,\n"passwordDictonary": true,\n"loginRetries": 3,\n"lockTimeInMinutes": 5\n}';
+    '{\n"isSecured": true,\n"passwordLength": 10,\n"complexPassword": true,\n"passwordHistoryLength": 3,\n"passwordDictonary": true,\n"loginRetries": 3,\n"lockTimeInMinutes": 5\n}';
 
   fs.writeFile("config/default.json", updatedJson, function (err) {
     if (err) throw err;
